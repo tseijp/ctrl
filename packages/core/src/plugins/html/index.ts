126c60465bdd084439296189434b53fed9e8305a
tseijp <_@tsei.jp>
2025-02-21 00:22:41 +0900

    chore: fix build error

---
+++ packages/core/src/plugins/html/index.ts
@@ -0,0 +1,15 @@
+import ctrl from '../../index'
+import { Attach } from '../../types'
+import { PluginItem } from '../index'
+
+const isHTML = (a: unknown): a is HTMLElement => {
+        return a instanceof HTMLElement
+}
+
+export function htmlPlugin(props: Attach<HTMLElement>) {
+        const { a, k } = props
+        if (!isHTML(a)) return null
+        const _ = ctrl.create
+        const c = ctrl(a, k)
+        return _(PluginItem, { c, ref: c.mount })
+}
